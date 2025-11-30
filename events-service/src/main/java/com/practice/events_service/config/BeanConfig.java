12de110f6f7174ef1ca22709ea1839029575a14a
hartmann78 <neizvestnik000@gmail.com>
2025-11-30 23:33:10 +0500

    stats-service host & port now can be changed in docker-compose.yaml

--- events-service/src/main/java/com/practice/events_service/config/BeanConfig.java
+++ events-service/src/main/java/com/practice/events_service/config/BeanConfig.java
@@ -2,14 +2,15 @@ package com.practice.events_service.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.practice.stats_client.StatsClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class BeanConfig {
     @Bean
-    public StatsClient statsClient() {
-        return new StatsClient(objectMapper());
+    public StatsClient statsClient(@Value("${stats.service.host}") String host, @Value("${stats.service.port}") int port) {
+        return new StatsClient(objectMapper(), host, port);
     }
 
     @Bean
--- stats-service-parent/stats-client/src/main/java/com/practice/stats_client/StatsClient.java
+++ stats-service-parent/stats-client/src/main/java/com/practice/stats_client/StatsClient.java
@@ -22,23 +22,16 @@ import java.time.format.DateTimeFormatter;
 @RequiredArgsConstructor
 public class StatsClient {
     private final ObjectMapper objectMapper;
+    private final String HOST;
+    private final int PORT;
     private static final String APP = "events-service";
 
-    private static String HOST;
-    private static final int PORT = 9090;
-
     public Boolean checkServiceAvailability() {
-        try (Socket ignored = new Socket("host.docker.internal", PORT)) {
-            HOST = "host.docker.internal";
+        try (Socket ignored = new Socket(HOST, PORT)) {
             return true;
-        } catch (IOException e1) {
-            try (Socket ignored = new Socket("localhost", PORT)) {
-                HOST = "localhost";
-                return true;
-            } catch (IOException e2) {
-                System.out.println("Сервис статистики недоступен!");
-                return false;
-            }
+        } catch (IOException e) {
+            System.out.println("Сервис статистики недоступен!");
+            return false;
         }
     }
 
