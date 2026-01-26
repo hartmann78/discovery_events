803087fe9f466cc19637b0da5f9f6fd451735020
hartmann78 <neizvestnik000@gmail.com>
2026-01-26 16:24:00 +0500

    changed 'data' annotation to getter & setter

--- events-service/src/main/java/com/practice/events_service/dto/other/Location.java
+++ events-service/src/main/java/com/practice/events_service/dto/other/Location.java
@@ -1,15 +1,13 @@
 package com.practice.events_service.dto.other;
 
 import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
-@Data
-@Builder
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class Location {
     @NotNull
     private Float lat;
